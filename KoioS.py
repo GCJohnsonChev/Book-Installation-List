c199fac00579793da12e05093ed52bd742e83054
Gary Christopher Johnson <47867437+GCJohnsonChev@users.noreply.github.com>
2022-08-24 11:08:04 -0700

    Update KoioS.py

--- KoioS.py
+++ KoioS.py
@@ -2,3 +2,6 @@ KoioS.py
 
 // an advanced python script for searching, collecting, storing and
 organizing digital Books, Digital Manuscripts and other digital materials.
+
+// Named after KoioS the Greek Titan and father of Asteria
+// Script written by G.C. Johnson
